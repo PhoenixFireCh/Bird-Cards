d349efcffb8fd03b94da7bfa22b2425abc7abd05
Harrison <harrisonch2023@gmail.com>
2026-06-11 17:58:04 -0700

    Adding visual elements

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,20 @@ import { use, useState } from 'react';
 
 const App = () => {
   const items = [
-    {imageSrc: './secretary bird.jpg', ans: 'Secretary Bird', difficulty: 'yellow'},
-    {imageSrc: './peregrine.jpg', ans: 'Peregrine Falcon', difficulty: 'green'},
-    {imageSrc: './miner.jpg', ans: 'Noisy Miner', difficulty: 'red'},
-    {imageSrc: './american crow.jpg', ans: 'American Crow', difficulty: 'green'},
-    {imageSrc: './red tail.jpg', ans: 'Red Tailed Hawk', difficulty: 'yellow'},
-    {imageSrc: './golden eagle.jpg', ans: 'Golden Eagle', difficulty: 'green'},
-    {imageSrc: './raven.jpg', ans: 'Common Raven', difficulty: 'red'},
+    {imageSrc: './secretary bird.jpg', ans: 'Secretary Bird', order: 'url(./acc.png)'},
+    {imageSrc: './peregrine.jpg', ans: 'Peregrine Falcon', order: 'url(./fal.png)'},
+    {imageSrc: './miner.jpg', ans: 'Noisy Miner', order: 'url(./pas.png)'},
+    {imageSrc: './american crow.jpg', ans: 'American Crow', order: 'url(./pas.png)'},
+    {imageSrc: './red tail.jpg', ans: 'Red Tailed Hawk', order: 'url(./acc.png)'},
+    {imageSrc: './golden eagle.jpg', ans: 'Golden Eagle', order: 'url(./acc.png)'},
+    {imageSrc: './raven.jpg', ans: 'Common Raven', order: 'url(./pas.png)'},
   ];
 
   const [reviewedCount, setReviewedCount] = useState(0);
   const [cardsReviewed, setCardsReviewed] = useState(Array(items.length).fill(0));
-  const [displayImg, setDisplayImg] = useState('none');
+  const [state, setState] = useState(false);
+  const [imgVisibility, setImgVisibility] = useState("none");
+  const [cardBackground, setCardBackground] = useState('none');
   const [mainText, setMainText] = useState("Can you guess the bird?");
   const [currAns, setCurrAns] = useState(mainText);
   const [currentImg, setCurrentImg] = useState(null);
@@ -22,44 +24,54 @@ const App = () => {
   const count = items.length;
 
   const flipCard = () => {
-    if (displayImg === 'none' && reviewedCount !== 0) {
-      setMainText("What's this bird?")
-      setDisplayImg("unset");
-    } else {
-      setMainText(currAns);
-      setDisplayImg("none");
-    }
+    setState(state => !state);
   }
 
   const nextCard = () => {
     let index = Math.floor(Math.random() * items.length);
+    //Increments counter
     if (cardsReviewed[index] == 0) {
       setReviewedCount(reviewedCount + 1);
       let updated = [...cardsReviewed];
       updated[index] = 1;
       setCardsReviewed(updated);
     }
+    //Changes background
+    setCardBackground(items[index].order);
+
     setCurrAns(items[index].ans);
+    setMainText("Whats this bird?");
     setCurrentImg(items[index].imageSrc);
-    setMainText("What's this bird?")
-    setDisplayImg("unset");
+    setImgVisibility("unset");
   }
+
+  const flip = {
+    transform: state ? "rotateY(0deg)" : "rotateY(180deg)",
+    transition: "transform 0.8s"
+  };
 
   return(
     <div className='App'>
       <div className='title'>
         <h1>Bird Cards!</h1>
         <h2>How good are you at guessing birbs!</h2>
-        <h3>Total card count: {count} </h3>
-        <h3>Total reviewed: {reviewedCount} </h3>
       </div>
+
+      <h3>{reviewedCount} out of {count} cards reviewed</h3>
 
       <div className="quiz">
         <div onClick={flipCard} className='card'>
-          <p>{mainText}</p>
-          <img style={{display: displayImg}} src={currentImg}></img>
+          <div className="flipCardInner" style={flip}>
+              <div className="flipCardFront" style={{backgroundImage: cardBackground}}>
+                <p><strong>{currAns}</strong></p>
+              </div>
+              <div className="flipCardBack" style={{backgroundImage: cardBackground}}>
+                <p><strong>{mainText}</strong></p>
+                <img style={{display: imgVisibility}} src={currentImg}></img>
+              </div>
+          </div>
         </div>
-        <button onClick={nextCard}>Next!</button>
+        <button onClick={nextCard}><strong>Next!</strong></button>
       </div>
     </div>
   
