6a13bbfa14872dd00baac6663a618a9f4e42c201
Harrison <harrisonch2023@gmail.com>
2026-06-18 22:31:47 -0700

    Flashcards p2 revamp initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,44 +11,62 @@ const App = () => {
     {imageSrc: './golden eagle.jpg', ans: 'Golden Eagle', order: 'url(./acc.png)'},
     {imageSrc: './raven.jpg', ans: 'Common Raven', order: 'url(./pas.png)'},
   ];
+  const initialCard = {imageSrc: 'none', ans: 'Can you guess the bird?', order: ''}
 
-  const [reviewedCount, setReviewedCount] = useState(0);
-  const [cardsReviewed, setCardsReviewed] = useState(Array(items.length).fill(0));
-  const [state, setState] = useState(false);
-  const [imgVisibility, setImgVisibility] = useState("none");
-  const [cardBackground, setCardBackground] = useState('none');
-  const [mainText, setMainText] = useState("Can you guess the bird?");
-  const [currAns, setCurrAns] = useState(mainText);
-  const [currentImg, setCurrentImg] = useState(null);
+  const [reviewedCount, setReviewedCount] = useState(0); //Amount of cards seen
+  const [currentIndex, setCurrentIndex] = useState(null); // Current index
+  const [currCard, setCurrCard] = useState(initialCard);
+  const [cardList, setCardList] = useState([...items]); // Allows things to be synced when shuffling
+  const [state, setState] = useState(false); // State the card is in, false meaning unflipped, true being flipped
 
-  const count = items.length;
+  const count = cardList.length;
 
   const flipCard = () => {
     setState(state => !state);
   }
 
+  const shuffle = () => {
+    let n = cardList;
+    for (let i = 0; i < n.length; i++) {
+      let r = Math.floor(Math.random() * n.length);
+      [n[i], n[r]] = [n[r], n[i]];
+    }
+    setCardList(n);
+    setCurrCard(n[0]);
+    setCurrentIndex(0);
+    setReviewedCount(1);
+  }
+
   const nextCard = () => {
-    let index = Math.floor(Math.random() * items.length);
+    let index = currentIndex;
+    if (currentIndex < count - 1) {
+      index++;
+    }
     //Increments counter
-    if (cardsReviewed[index] == 0) {
+    if (reviewedCount < count) {
       setReviewedCount(reviewedCount + 1);
-      let updated = [...cardsReviewed];
-      updated[index] = 1;
-      setCardsReviewed(updated);
     }
     //Changes background
-    setCardBackground(items[index].order);
+    setCurrentIndex(index);
+    setCurrCard(cardList[index]);
+  }
 
-    setCurrAns(items[index].ans);
-    setMainText("Whats this bird?");
-    setCurrentImg(items[index].imageSrc);
-    setImgVisibility("unset");
+  const prevCard = () => {
+    let index = currentIndex;
+    if (currentIndex > 0) {
+      index--;
+    }
+    setCurrentIndex(index);
+    setCurrCard(cardList[index]);
   }
 
   const flip = {
     transform: state ? "rotateY(0deg)" : "rotateY(180deg)",
     transition: "transform 0.8s"
   };
+  const imgVisibility = currCard.imageSrc == 'none' ? 'none': 'unset';
+  const mainText = currCard.imageSrc == 'none' ? "Can you guess the bird?" : "Whats this bird?";
+
 
   return(
     <div className='App'>
@@ -62,16 +80,25 @@ const App = () => {
       <div className="quiz">
         <div onClick={flipCard} className='card'>
           <div className="flipCardInner" style={flip}>
-              <div className="flipCardFront" style={{backgroundImage: cardBackground}}>
-                <p><strong>{currAns}</strong></p>
-              </div>
-              <div className="flipCardBack" style={{backgroundImage: cardBackground}}>
+              <div className="flipCardFront" style={{backgroundImage: currCard.order}}>
                 <p><strong>{mainText}</strong></p>
-                <img style={{display: imgVisibility}} src={currentImg}></img>
+                <img style={{display: imgVisibility}} src={currCard.imageSrc}></img>
+              </div>
+              <div className="flipCardBack" style={{backgroundImage: currCard.order}}>
+                <p><strong>{currCard.ans}</strong></p>
               </div>
           </div>
         </div>
-        <button onClick={nextCard}><strong>Next!</strong></button>
+        <form className="formBox">
+          <input placeholder="Make a guess!" type="text"></input>
+          <input type="submit" className='submit'></input>
+        </form>
+
+        <div className='actionBox'>
+          <button onClick={prevCard}><strong>{'<'}</strong></button>
+          <button onClick={nextCard}><strong>{'>'}</strong></button>
+          <button onClick={shuffle}><strong>Shuffle</strong></button>
+        </div>
       </div>
     </div>
   
